662ed1dc33549f002388cde264e21ac1000dfb3a
JulienDörner <julien.doerner@rub.de>
2023-03-10 14:11:13 +0100

    update new positions in correct coordinate system for CMZDensity
    the position of the molecular clouds are updated and restrucred for better readability.
    For now the central 10 pc structure (Ferriere 2012)  is missing.

--- include/crpropa/massDistribution/CMZDensity.h
+++ include/crpropa/massDistribution/CMZDensity.h
@@ -14,15 +14,28 @@ namespace crpropa {
     
     class CMZDensity: public Density {
     private: 
-        bool useClouds;
-        
+        bool useClouds, useDiffuse;
+
+        // parameters for the molecular clouds
+        std::vector<double> MC_density = {1.8e10, 1e10, 1e10, 1.3e10, 7e9, 9e9, 8e9, 2.8e10, 3.2e10, 3e9, 6e9 };
+        std::vector<double> MC_radius = {1.7, 9.4, 4.5, 2.4, 1.9, 1.9, 3.3, 3.5, 2.4, 10, 1.8}; // in pc
+        std::vector<Vector3d> MC_orig;
+
     public:
-        CMZDensity(bool useClouds = true);
+
+        double cloudDensity(const Vector3d &position, const Vector3d &center, double radius, double density) const;
+
+        CMZDensity(bool useClouds = true, bool useDiffuse = true);
         double getDensity(const Vector3d &position) const;
         double getHIDensity(const Vector3d &position) const;
         double getH2Density(const Vector3d &position) const;
         double getNucleonDensity(const Vector3d &position) const;
         
+        void printPositions() {
+            for(int i = 0; i < MC_orig.size(); i++) {
+                std::cout << "number i = "<< i << " at position " << MC_orig[i] << "\n";
+            }
+        }
     };
     
 } // namespace
