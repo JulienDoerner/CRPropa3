1c08c0b33ce9960b78ee6f32ff75b8a6852b1ca1
JulienDörner <julien.doerner@rub.de>
2023-03-13 12:33:34 +0100

    update positions in CMZField for the correct cooridnate system

--- src/magneticField/CMZField.cpp
+++ src/magneticField/CMZField.cpp
@@ -98,89 +98,89 @@ void CMZField::setUseRadioArc(bool use) {
 }
 
 Vector3d CMZField::getMCField(const Vector3d& pos) const {//Field in molecular clouds
-    Vector3d b(0.);
+    Vector3d b(0.); // calculation in milli Gauss
     double eta=0.01;
     double N=59; // normalization factor, depends on eta
 
 	// azimuthal component in dense clouds
     //A=SgrC 
-    Vector3d mid(0,-81.59*pc, -16.32*pc);
+    Vector3d mid(0, 81.60 * pc, -16.32 * pc);
     double R = 1.7*pc; 
-    double B1=2.1e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    double B1 = 0.6;  
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     // A=G0.253+0.016 Dust Ridge A
-    mid = Vector3d(0, 37.53*pc, -2.37*pc);
-    R=2.4*pc; 
-    B1=2.5e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, - 37.53 * pc, 2.37 * pc);
+    R = 2.4*pc; 
+    B1 = 0.5;
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     //A=Dust Ridge B
-    mid = Vector3d(0, 50.44, 8.16)*pc;
-    R=1.9*pc; 
-    B1=0.9e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -50.44, 8.16) * pc;
+    R = 1.9 * pc; 
+    B1 = 1.3;
+    b += BAz(pos, mid, B1 / N, eta, R);
     
     //A=Dust Ridge C
-    mid = Vector3d(0,56.37,7.71)*pc;
-    R=1.9*pc; 
-    B1=1.2e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -56.37, 7.42) * pc;
+    R = 1.9 * pc; 
+    B1 = 0.4;
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     //A=Dust Ridge D
-    mid = Vector3d(0, 60.82, 7.42)*pc;
-    R=3.3*pc; 
-    B1=1.7e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -61.12, 7.71) * pc;
+    R = 3.3 * pc; 
+    B1 = 0.4;
+    b += BAz(pos, mid, B1 / N, eta, R);
     
     //A=Dust Ridge E
-    mid = Vector3d(0, 70.91, 0.74)*pc;
-    R=3.5*pc; 
-    B1=4.1e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -70.91, 0.74) * pc;
+    R = 3.5 * pc; 
+    B1 = 0.7;
+    b += BAz(pos, mid, B1 / N, eta, R);
         
     //A=Dust Ridge F
-    mid = Vector3d(0, 73.58, 2.97)*pc;
-    R=2.4*pc; 
-    B1=3.9e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -73.58, 2.97) * pc;
+    R = 2.4 * pc; 
+    B1 = 0.9;
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     //Sgr D
-    mid = Vector3d(0, 166.14, -10.38)*pc;
-    R=1.8*pc;
-    B1=0.8e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -167.66, -16.32) * pc;
+    R = 1.8 * pc;
+    B1= 0.2;
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     //Sgr B2
-    mid = Vector3d(0, 97.01, -5.93)*pc;
-    R=14*pc; 
-    B1=1.0e-3/N;
-    b += BAz(pos, mid, B1, eta, R);
+    mid = Vector3d(0, -97.92, -5.93) * pc;
+    R = 10 * pc; 
+    B1 = 1.3;
+    b += BAz(pos, mid, B1 / N, eta, R);
         
     //A=Inner R=5pc
-    mid = Vector3d(0, -8.3, -6.9)*pc;
-    R=5*pc; 
-    B1=3.0e-3/0.91;    
-    b += BAz(pos, mid, B1, 0.77, R); // different eta value!
+    mid = Vector3d(0, 8.31, -6.82) * pc;
+    R = 5 * pc; 
+    B1 = 8.4;
+    b += BAz(pos, mid, B1 / N, eta, R);
 
     //20 km s^-1
-    mid = Vector3d(0, -19.29,-11.87)*pc;
-    R=9.4*pc; 
-    B1=2.7e-3/N;
-    b += BAz(pos, mid, B1, eta, R);    
+    mid = Vector3d(0, 19.29, -11.87) * pc;
+    R = 9.4 * pc; 
+    B1 = 0.9;
+    b += BAz(pos, mid, B1 / N, eta, R);    
     
     //50 km s^-1
-    mid = Vector3d(0, -2.97, -10.38)*pc;
-    R=9.4*pc; 
-    B1=3.7e-3/N;
-    b += BAz(pos, mid, B1, eta, R);    
+    mid = Vector3d(0, 2.97, -10.38) * pc;
+    R = 4.5 * pc;
+    B1= 1.3;
+    b += BAz(pos, mid, B1 / N, eta, R);    
     
     //SgrA* is different orrientated! 
     //only phi component
     double x = pos.x;
-    double y = pos.y + 8.3*pc;
-    double z = pos.z + 6.9*pc;
-    R=1.2e12; 
+    double y = pos.y - 8.31 * pc;
+    double z = pos.z + 6.82 * pc;
+    R=1.2e12 * meter; 
     B1=65./3.07;
     double Hc = R/sqrt(log(2));
     double r = sqrt(x*x + y*y);
@@ -198,11 +198,11 @@ Vector3d CMZField::getMCField(const Vector3d& pos) const {//Field in molecular c
     b.x -= Bphi*sin(phi);
     b.y += Bphi*cos(phi);
 
-    return b*gauss;
+    return b * milli * gauss;
 } 
 
 Vector3d CMZField::getICField(const Vector3d& pos) const {//Field in intercloud medium--> poloidal field
-    Vector3d mid(0.,-8.3*pc,-6.9*pc);
+    Vector3d mid(0., 8.31 * pc, -6.82 * pc); // centered in SgrA*
 
     double eta = 0.85;
     double B1 = 1e-5*gauss;
@@ -211,77 +211,77 @@ Vector3d CMZField::getICField(const Vector3d& pos) const {//Field in intercloud 
     double L = 158*pc/log(2);
 
     return BPol(pos, mid, B2, a, L);
-}                                                         
+}
   
 Vector3d CMZField::getNTFField(const Vector3d& pos) const {//Field in the non-thermal filaments--> predominantly poloidal field (except "pelical"-> azimuthal)
-    Vector3d b(0.); 
+    Vector3d b(0.); // units in muG
     Vector3d mid(0.);
 
     //A=SgrC
-    mid = Vector3d(0., -81.59,-1.48)*pc;
-    double a1=27.44*pc;
-    double a2=1.73*pc;
+    mid = Vector3d(0., 81.60, -1.48) * pc;
+    double a1=27.4 * pc;
+    double a2=1.7 * pc;
     double eta=0.48;
-    double B1=1.e-4;
+    double B1=100;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
     //A=G359.15-0.2 The Snake
-    mid = Vector3d(0, -126.1,-25.22)*pc;
-    a1=12.86*pc;
-    a2=2.22*pc;
-    B1=88.e-6;
+    mid = Vector3d(0, 126.11, -25.22) * pc;
+    a1=12.9 * pc;
+    a2=1.5 * pc;
+    B1=88;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
-    //A=G359.54+0.18 Nonthermal Filament
-    mid = Vector3d(0, -68.24,25.22)*pc;
-    a1=15.08*pc;
-    a2=2.72;
-    B1=1.e-3;
+    //A=G359.54+0.18 Nonthermal Filament (Ripple)
+    mid = Vector3d(0, 66.76, 25.22) * pc;
+    a1=15.0 * pc;
+    a2=2.7 * pc;
+    B1=1000;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
     //A=G359.79 +17 Nonthermal Filament
-    mid = Vector3d(0,-31.15,23.74)*pc;
-    a1=16.07*pc;
-    a2=3.46*pc;
-    B1=1.e-3;
+    mid = Vector3d(0, 29.67, 23.74) * pc;
+    a1=16.2 * pc;
+    a2=3.5 * pc;
+    B1=1000;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
     //A=G359.96 +0.09  Nonthermal Filament Southern Thread
-    mid = Vector3d(0, 5.93, 16.32)*pc;
-    a1=28.68*pc;
-    a2=1.73*pc;
-    B1=1.e-4;
+    mid = Vector3d(0, 5.93, 16.32) * pc;
+    a1=28.7 * pc;
+    a2=1.7 * pc;
+    B1=100;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
     //A=G0.09 +0.17  Nonthermal Filament Northern thread
-    mid = Vector3d(0, 13.35, 25.22)*pc;
-    a1=29.42*pc;
-    a2=2.23*pc;
-    B1=140.e-6;
+    mid = Vector3d(0, -13.35, 25.22) * pc;
+    a1=29.4 * pc;
+    a2=2.0 * pc;
+    B1=140;
     b += BPol(pos, mid, B1/eta, getA(a1), getL(a2));
 
     //A=G359.85+0.47  Nonthermal Filament The Pelican  is not poloidal but azimuthal
-    mid = Vector3d(0, -22.25, 69.73)*pc;
-    a1=11.37*pc;
-    a2=2.23*pc;
-    B1=70.e-6 /eta;
+    mid = Vector3d(0, 22.25, 69.73) * pc;
+    a1=13.4 * pc;
+    a2=2.2 * pc;
+    B1=70; 
     // by and bz switched because pelican is differently oriented
     Vector3d bPelican = BPol(pos, mid, B1/eta, getA(a1), getL(a2));
     b.x += bPelican.x;
     b.y += bPelican.z;
     b.z += bPelican.y;
     
-	return b*gauss;
+	return b*muG;
 }
   
 Vector3d CMZField::getRadioArcField(const Vector3d& pos) const {//Field in the non-thermal filaments--> predominantly poloidal field
     //poloidal field in the non-thermal filament region A=RadioArc
     double eta=0.48;
-    Vector3d mid(0,26.7*pc,10.38*pc);
-    double a1=70.47*pc;// arcmin-> deg->cm
-    double a2=9.89*pc;// arcmin-> deg-> cm
-    double B1=1.e-3;
-    return BPol(pos, mid, B1/eta, getA(a1), getL(a2))*gauss;
+    Vector3d mid(0, -26.70 * pc, -10.38 * pc);
+    double a1=70.5 * pc;
+    double a2=9.9 * pc;
+    double B1=1000;
+    return BPol(pos, mid, B1/eta, getA(a1), getL(a2)) * muG;
 }
 
 Vector3d CMZField::getField(const Vector3d& pos) const{
