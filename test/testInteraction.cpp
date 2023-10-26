5435eb3fd43d31dcd3ec4b19f832be3346d7d13f
JulienDörner <julien.doerner@rub.de>
2023-10-26 17:32:55 +0200

    extact field from vector before passing to module

--- test/testInteraction.cpp
+++ test/testInteraction.cpp
@@ -754,11 +754,13 @@ TEST(EMPairProduction, secondaries) {
 	std::cout << " / second";
 	fields.push_back(URB);
 	std::cout << " / thrid field to list\n";
+	std::cout << "have in total " fields.size() << "fields\n";
 
 	// loop over photon backgrounds
 	for (int f = 0; f < fields.size(); f++) {
 		std::cout << "start loop f = " << f << "\n";
-		m.setPhotonField(fields[f]);
+		ref_ptr<PhotonField> field = fields[f];
+		m.setPhotonField(field);
 		for (int i = 0; i < 140; i++) { // loop over energies Ep = (1e10 - 1e23) eV
 			double Ep = pow(10, 9.05 + 0.1 * i) * eV;
 			Candidate c(22, Ep);
